db6281208b0d2efa5cc5adc89e40e0d1f27bdf67
MarcelinTeodoro <137842006+MarcelinTeodoro@users.noreply.github.com>
2024-03-24 14:42:53 -0300

    ex5

--- Lista 2/500/Operação5.java
+++ Lista 2/500/Operação5.java
@@ -1,5 +1,76 @@
-
+import javax.swing.JOptionPane;
 
 public class Operação5 {
+    int vetor1[] = new int[5];
+    int vetor2[] = new int[5];
+    
+    
+    boolean statusVetor1 = false;
+    boolean statusVetor2 = false;
+
+
+    public boolean criarVetor1(){
+        
+        int b = 1;
+        for(int i = 0; i < 5; i++){
+            String  valor = JOptionPane.showInputDialog("Digiti numero "+ b +":");
+            int entrada = Integer.parseInt(valor);
+            this.vetor1[i] = entrada;
+            b++;
+        }
+        statusVetor1 =  true;
+        return true;
+        
+    }
+    public boolean criarVetor2(){
+        
+        int b = 1;
+        for(int i = 0; i < 5; i++){
+            String  valor = JOptionPane.showInputDialog("Digiti numero "+ b +":");
+            int entrada = Integer.parseInt(valor);
+            this.vetor2[i] = entrada;
+            b++;
+        }
+        statusVetor2 =  true;
+        return true;
+    }
+
+    void somar(){
+        if(statusVetor2 == true && statusVetor1 == true ){
+            System.out.println("Soma");
+            for(int i = 0; i < 5; i++){
+                System.out.println(vetor1[i] + vetor2[i]);
+            }
+        }else{
+            JOptionPane.showMessageDialog(null, "Deve criar os dois vetores do caso 1 e 2");
+        }
+    }
+
+    void subtrair(){
+        if(statusVetor2 == true && statusVetor1 == true ){
+            System.out.println("Diferença");
+            for(int i = 0; i < 5; i++){
+                System.out.println(vetor1[i] - vetor2[i]);
+            }
+        }else{
+            JOptionPane.showMessageDialog(null, "Deve criar os dois vetores do caso 1 e 2");
+        }
+    }
+
+    void imprimir(){
+        if( statusVetor2 == true && statusVetor1 == true ){
+            System.out.println("Vetor A");
+            for(int i = 0; i < 5; i++){
+                System.out.println(this.vetor1[i]);
+            }
+            System.out.println("Vetor B");
+            for(int i = 0; i < 5; i++){
+                System.out.println(this.vetor2[i]);
+            }
+        
+        }else{
+            JOptionPane.showMessageDialog(null, "Deve criar os dois vetores do caso 1 e 2");
+        }
+    }
     
 }
--- Lista 2/500/Teste5.java
+++ Lista 2/500/Teste5.java
@@ -1,5 +1,40 @@
-
+import javax.swing.JOptionPane;
 
 public class Teste5 {
-    
+    public static void main(String[] args) {
+Operação5 op1 = new Operação5();
+
+        while (true) {
+            String  valor = JOptionPane.showInputDialog("imeiros  o wfsdfgoritimo");
+            int entrada = Integer.parseInt(valor);
+        
+        
+            switch (entrada) {
+            case 1:
+                op1.criarVetor1();
+                break;
+            
+            case 2:
+                op1.criarVetor2();
+
+                break;
+            
+            case 3:
+                op1.imprimir();
+                break;
+            case 4:
+                op1.somar();
+                break;
+            case 5:
+                op1.subtrair();
+                break;
+            case 6:
+                
+                System.exit(0);
+                break;
+            
+            
+            }
+        }
+    }
 }
