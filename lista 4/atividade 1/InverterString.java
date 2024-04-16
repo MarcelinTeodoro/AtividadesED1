284c7b59fb889b97a6909965e69f29f2aa337666
MarcelinTeodoro <137842006+MarcelinTeodoro@users.noreply.github.com>
2024-04-16 14:33:49 -0300

    InvertendoString

--- lista 4/atividade 1/InverterString.java
+++ lista 4/atividade 1/InverterString.java
@@ -2,31 +2,30 @@ import java.util.Stack;
 
 public class InverterString {
 
-    /*Implementar uma estrutura de Pilha em Java. O programa da pilha deve inverter a ordem das letras
-    de cada palavra de uma string ASCII, preservando a ordem das palavras (acentos podem ser desconsiderados).
-    Por exemplo, para a string  ESTE EXERCICIO E MUITO FACIL  o resultado deve ser  ETSE OICICREXE E OTIUM LICAF.
-
-use os dois exemplos abaixo:
-
-1) UM CIENTISTA DA COMPUTAÇAO OU UM TECNÓLOGO EM SISTEMAS PARA INTERNET DEVE RESOLVER OS PROBLEMAS LOGICAMENTE
-
-2) ESARF :ATERCES ODALERAHCAB ME AICNEIC AD OAÇATUPMOC  E AIGOLONCET ME SAMETSIS ARAP TENRETNI OD FI ONAIOG SUPMAC 
-SOHNIRROM OAS SOD SEROHLEM SOSRUC ED OAÇATUPMOC OD ODATSE ED SAIOG */
+    
     
     public static void main(String[] args) {
-        Stack <String> pilha;
-        pilha = new Stack<String>();
+        Stack <Character> pilha;
+        pilha = new Stack<>();
 
-        String T = " ESTE EXERCICIO E MUITO FACIL";
+        String frase = "ESARF :ATERCES ODALERAHCAB ME AICNEIC AD OAÇATUPMOC  E AIGOLONCET ME SAMETSIS ARAP TENRETNI OD FI ONAIOG SUPMAC SOHNIRROM OAS SOD SEROHLEM SOSRUC ED OAÇATUPMOC OD ODATSE ED SAIOG";
         
-        String []palavras = T.split(" ");
-        for (String palavra : palavras){
-            
-            for (int i = 0; i < palavra.length(); i++) {
-                pilha.push( = palavra.charAt(i));
-                
-}
+        
+        for(int i = 0 ; i < frase.length(); i++){
+            pilha.push(frase.charAt(i));
+            if(frase.charAt(i) == ' '){
+                pilha.push(frase.charAt(i));
+                while (!pilha.empty()) {
+                    System.out.print(pilha.pop());
+                }
+            }
 
+        } 
+        System.out.print(" ");
+        while (!pilha.empty()) {
+            System.out.print(pilha.pop());
         }
+
+
     }
 }
